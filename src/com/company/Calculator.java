e240ad903b34f8bc73008e02d51ee55eee4ede02
danilvolk <danilvolk93@gmail.com>
2022-02-22 21:49:29 +0500

    Refinement

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -7,9 +7,9 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) ->  x / y; // Деление на ноль
+    //BinaryOperator<Integer> devide = (x, y) ->  x / y; // Деление на ноль
     // Другой способ решения  проблемы
-    // BinaryOperator<Integer> devide = (x, y) -> y == 0 ? (x >= 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE) :  x / y;
+    BinaryOperator<Integer> devide = (x, y) -> y == 0 ? (x >= 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE) :  x / y;
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPositive = x -> x > 0;
